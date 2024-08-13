259db610b80aa1855739697318f1db43b4026708
Shyamraj V S <shyamrajvs737@gmail.com>
2024-08-14 00:48:02 +0530

    forth commit

---
+++ Array/arrayIntroduction.js
@@ -0,0 +1,35 @@
+//Here’s a comprehensive list of array methods in JavaScript, along with brief descriptions of what they do:
+
+//Mutating Methods
+
+/*1.push(element1, element2, ...)(https://github.com/shyamrajvs/Data_Structures-Algorithms/blob/main/Array/Mutating%20Methods/push.js)
+
+    Adds one or more elements to the end of the array and returns the new length.
+
+2.pop()
+
+    Removes the last element from the array and returns it.
+
+3.shift()
+
+    Removes the first element from the array and returns it.
+
+4.unshift(element1, element2, ...)
+
+    Adds one or more elements to the beginning of the array and returns the new length.
+
+5.splice(start, deleteCount, item1, item2, ...)
+
+    Changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+
+6.fill(value, start, end)
+
+    Fills all the elements of an array from a start index to an end index with a static value.
+
+7.reverse()
+
+    Reverses the elements of an array in place.
+
+8.sort(compareFunction)
+
+    Sorts the elements of an array in place and returns the array.*/
--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 Welcome to the **Data Structures & Algorithms** repository! This project is designed to provide a comprehensive collection of data structures and algorithms implemented in JavaScript. It serves as both a learning resource and a practical reference for developers.
 
-## Table of Contents
-
-- [Array](https://github.com/shyamrajvs/Data_Structures-Algorithms/tree/main/Array)
-
 ## Introduction
 
 This repository contains various data structures and algorithms implemented in JavaScript. It covers fundamental concepts and advanced topics to help you understand and apply these techniques in your projects.
 - [Introduction](https://github.com/shyamrajvs/Data_Structures-Algorithms/blob/main/Introduction.js)
+
+## Table of Contents
+
+- [Array](https://github.com/shyamrajvs/Data_Structures-Algorithms/tree/main/Array)
+
+
 
 ## Getting Started
 
